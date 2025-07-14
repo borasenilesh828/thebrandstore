3e1d673bb8aa263118d88da86414830749f300d7
Nilesh Borase <158148138+borasenilesh828@users.noreply.github.com>
2025-07-14 15:27:25 +0530

    Add files via upload

---
+++ server/config/database.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  mongoURI: 'mongodb+srv://nileshborase22:Nilesh%40123@cluster0.1ixsar9.mongodb.net/thebrandstore?retryWrites=true&w=majority&appName=Cluster0',
+  port: 4000
+};
+
---
+++ server/config/secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+    api_key: 'your_api_key_here',
+    client_secret: 'your_client_secret_here',
+  };
+  
